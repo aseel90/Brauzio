8213484e8f1cb86deb5018a86db8e672bd7950b9
aseel salah <aseel.salah90@yahoo.com>
2026-09-06 23:50:38 +0200

    Expose event watch tools through shared schema

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,16 @@
 export * from './types';
-export * from './tools';
+
+import { TOOL_NAMES as BASE_TOOL_NAMES, TOOL_SCHEMAS as BASE_TOOL_SCHEMAS } from './tools';
+import { WATCH_TOOL_NAMES, WATCH_TOOL_SCHEMAS } from './watch-tools';
+
+export const TOOL_NAMES = {
+  ...BASE_TOOL_NAMES,
+  BROWSER: {
+    ...BASE_TOOL_NAMES.BROWSER,
+    ...WATCH_TOOL_NAMES,
+  },
+} as const;
+
+export const TOOL_SCHEMAS = [...BASE_TOOL_SCHEMAS, ...WATCH_TOOL_SCHEMAS];
+
+export { WATCH_TOOL_NAMES, WATCH_TOOL_SCHEMAS } from './watch-tools';
