36ea29450d352303bb786186616ea88d19d1a9b4
aseel salah <aseel.salah90@yahoo.com>
2026-09-06 16:05:40 +0200

    fix: decouple welcome page from legacy Tailwind UI

--- app/chrome-extension/entrypoints/welcome/main.ts
+++ app/chrome-extension/entrypoints/welcome/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
-// Tailwind first, then custom tokens
-import '../styles/tailwind.css';
 import '../../brand.css';
 
 createApp(App).mount('#app');
